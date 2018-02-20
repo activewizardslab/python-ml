9b10ea870967dda86595a91a2983ea902dfaf6a5
Volodymyr Rospotniuk <rospotnyk@ukr.net>
2018-02-20 11:59:19 +0200

    Update tweets_classifier.scala

--- tweets_classifier.scala
+++ tweets_classifier.scala
@@ -8,7 +8,6 @@ import org.apache.spark.mllib.linalg.Vector
 
 
 // get data from hdfs
-
 val tweet = sc.textFile("wasb://bigdata01@bigdatastore01.blob.core.windows.net/user/CentrikAlpha01/tweets_words.csv")
 tweet.take(10)
 
@@ -24,7 +23,6 @@ val parsedData = tweet.map { line =>
 }
 
 
-
 // Split data into training (60%) and test (40%).
 val splits = parsedData.randomSplit(Array(0.6, 0.4), seed = 11L)
 val training = splits(0)
@@ -32,7 +30,6 @@ val test = splits(1)
 
 val model = NaiveBayes.train(training, lambda = 1.0)
 val prediction = model.predict(test.map(_.features))
-
 
 for (i <- prediction) {
   print(i)
@@ -45,11 +42,6 @@ for (i <- prediction) {
 val predictionAndLabel = prediction.zip(test.map(_.label))
 val accuracy = 1.0 * predictionAndLabel.filter(x => x._1 == x._2).count() / test.count()
 
-
-
-
-
-
 val parsedData_test = tweet_test.map { line =>
   val parts = line.split(',')
   hashingTF.transform(parts)
@@ -58,11 +50,11 @@ val parsedData_test = tweet_test.map { line =>
 val prediction_test = model.predict(parsedData_test)
 prediction_test.take(10)
 
-
 for (i <- prediction_test) {
   print(i)
   if (i == 1) println("   Personal Life Event in this tweet is : WEDDING") 
   else if (i == 2 ) println (",   Personal Life Event in this tweet is : GRADUATE")
   else if (i == 3 ) println (",   Personal Life Event in this tweet is : NEW JOB") 
   else if (i == 4 ) println (",   Personal Life Event in this tweet is : BIRTH IN FAMILIE") 
-  else if (i == 0 ) println (",   Personal Life Event in this tweet is: OTHER")}
+  else if (i == 0 ) println (",   Personal Life Event in this tweet is: OTHER")
+}
